573872671ab2ba38a9169158a1844b7d2bada2b7
jireh <jirehsiayngco001@gmail.com>
2022-07-20 17:26:30 -0600

    feat: added quiz component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
 import React from "react";
 import StartQuiz from "./components/StartQuiz";
 import Quiz from "./components/Quiz";
+import { nanoid } from "nanoid";
 
 export default function App() {
-  /* State is responsible for starting the quiz
-  false = load <Quiz />, true = load <StartQuiz /> */
-  const [startState, setStartState] = React.useState(false);
+  const [startQuiz, setStartQuiz] = React.useState(false); //change back to false
+  const [questions, setQuestions] = React.useState([]);
+  const [score, setScore] = React.useState(0);
+
+  // console.log(questions);
+
+  const quizElements = questions.map((question) => {
+    return (
+      <Quiz
+        key={question.id}
+        id={question.id}
+        question={question.title}
+        answers={question.answers}
+        correctAnswer={question.correctAnswer}
+        isSelected={question.isSelected}
+      />
+    );
+  });
+
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5")
+      .then((res) => res.json())
+      .then((data) =>
+        setQuestions(
+          data.results.map((question) => {
+            return {
+              id: nanoid(),
+              title: question.question,
+              answers: question.incorrect_answers.concat(
+                question.correct_answer
+              ),
+              correctAnswer: question.correct_answer,
+              isSelected: false,
+            };
+          })
+        )
+      );
+  }, []);
 
   function loadQuiz() {
-    setStartState((prevStart) => !prevStart);
+    setStartQuiz((prevStart) => !prevStart);
   }
 
   return (
     <div className="quizzical">
-      {startState ? <Quiz /> : <StartQuiz loadQuiz={loadQuiz} />}
+      {startQuiz ? (
+        <div className="quiz">
+          {quizElements}
+          <div className="button-container">
+            <button className="quiz-button">Check answers</button>
+          </div>
+        </div>
+      ) : (
+        <StartQuiz loadQuiz={loadQuiz} />
+      )}
     </div>
   );
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-export default function Quiz() {
+export default function Quiz(props) {
+  const answers = props.answers.map((answer) => {
+    return <button className="quiz-answer">{answer} </button>;
+  });
+
   return (
     <div>
-      <h1>Trivia Questions</h1>
+      <h2 className="quiz-question">{props.question}</h2>
+      <div className="answers">{answers}</div>
+      <hr className="separator" />
     </div>
   );
 }
+
+/* <h2 className="quiz-question">How would one say goodbye in Spanish?</h2>
+<div className="answers">
+  <button className="quiz-answer">Adios</button>
+  <button className="quiz-answer">Hola</button>
+  <button className="quiz-answer">Au Revoir</button>
+  <button className="quiz-answer">Ciao</button>
+</div>
+<hr className="separator" /> */
